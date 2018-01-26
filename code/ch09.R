644b1b6a164dbbdc1c34043cf258990ef00c29b8
90kim <kimgy9090@gmail.com>
2018-01-26 21:59:18 +0900

    chapter09 - Classification Algorithm

--- code/ch09.R
+++ code/ch09.R
@@ -87,3 +87,101 @@ nearZeroVar(Soybean) # Index number of Column
 mySoybean <- Soybean[,-nearZeroVar(Soybean)]
 
 ### Correlation 
+data("Vehicle")
+head(Vehicle)
+str(Vehicle)
+findCorrelation(cor(subset(Vehicle, select = -c(Class))))
+
+# install.packages("FSelector")
+library(FSelector)
+data(Ozone)
+head(Ozone)
+v <- linear.correlation(V4 ~ ., data = subset(Ozone, select = -c(V1,V2,V3)))
+v
+cutoff.k(v, 5)
+
+### Chi-Squared Test
+library(FSelector)
+library(mlbench)
+data("Vehicle")
+cs <- chi.squared(Class ~., data = Vehicle)
+cs
+
+### Model Validation 
+predicted <- c(1,0,0,1,1,1,0,0,0,1,0,0)
+actual <- c(1,0,0,1,1,0,1,1,0,0,0,0)
+xtabs(~predicted+actual)
+
+confusionMatrix(predicted, actual)
+
+set.seed(137)
+probs <- runif(100)
+labels <- as.factor(ifelse(probs>0.5 & runif(100)<0.4, "A", "B"))
+# install.packages("ROCR")
+library(ROCR)
+pred <- prediction(probs, labels)
+plot(performance(pred, "tpr", "fpr"))
+performance(pred, "auc")
+
+
+### Cross validation
+# install.packages("cvTools")
+library(cvTools)
+cvFolds(10, 5, type = "random")
+cvFolds(10, 5, type = "consecutive")
+
+set.seed(719)
+cv <- cvFolds(NROW(iris), K=10, R=3)
+cv
+head(cv$which, 30)
+head(cv$subsets)
+
+library(foreach)
+set.seed(719)
+R = 3
+K = 10
+cv <- cvFolds(NROW(iris), K=K, R=R)
+
+# foreach(r=1:R) %do% {
+#   foreach(k=1:K, .combine = c) %do% {
+#     validation_idx <- cv$subsets[which(cv$which == k), r]
+#     train <- iris[-validation_idx,]
+#     validation <- iris[validation_idx,]
+#     # Data preprocessing
+#     
+#     
+#     # train model
+#     
+#     
+#     # predictrion
+#     
+#     
+#     # performance evaluation
+#     # return(performance value)
+#   }
+# }
+
+# createDataPartition(), createFolds(), createMultiFolds()
+parts <- createDataPartition(iris$Species, p=0.8) #iris의 80%를 훈련데이터로 쓸거임 #분류기준은 Species
+parts
+table(iris[parts$Resample1, "Species"]) #분류기준별로 몇개의 훈련 데이터가 뽑혓는지 확인
+table(iris[-parts$Resample1, "Species"]) #분류기준별로 몇개의 테스트 데이터가 뽑혓는지 확인 
+createFolds(iris$Species, k=10)
+createMultiFolds(iris$Species, k=10, times=3)
+
+k <- 10
+times <- 3
+set.seed(137)
+cv <- createMultiFolds(iris$Species, k, times)
+
+# for (i in 1:times){
+#   for (j in 1:k){
+#     train_idx <- cv[[i*times + k]] ------------------ i*times + k ?? 확인해볼 것 !
+#     iris_train <- iris[train_idx,]
+#     iris_validation <- iris[-train_idx,]
+#     # modeling
+#     # ...
+#     # evaluation
+#     # ...
+#   }
+# }
